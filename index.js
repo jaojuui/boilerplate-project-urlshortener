a625da65d467d960edf0ee2dca6072c9746c2345
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-09 21:52:35 +0700

    update

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ app.get("/", function (req, res) {
 app.post("/api/shorturl", (req, res) => {
   try {
     const host = new URL(req.body.url);
+    if (parsedUrl.protocol !== "http:" && parsedUrl.protocol !== "https:") {
+      return res.json({ error: "invalid url" });
+    }
     const hostName = host.hostname;
     url_all[index] = req.body.url;
+
     dns.lookup(hostName, (err, address) => {
       if (err) return console.log(err);
       res.json({ original_url: host, short_url: index });
